70028aa6be238773e82e7982d0181e2fabfd0274
Qim <qimqimqim@proton.me>
2023-03-01 22:53:19 +0800

    add README and origin web site

---
+++ origin/《人类减贫的中国实践》白皮书（全文）_files/SCIOSiteStatis.aspx
@@ -0,0 +1,8 @@
+document.writeln("<iframe src='http://www.scio.gov.cn:9095/Statis/ActualStatis.aspx?key=CBYAHFXHVBAJJDIJNDTEFBDFCGNIAEXCKFTASEOEXDWELGNAWFOCXFYIHFRFGJGERAUCBBCFQJTEKGZFBJ&SiteID=27&Kind=Site&nOutput=0&OneDay=&Pick=True' style='width:1px;height:1px;display:none'></iframe>");
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?7cd4be5b075ad771f065c6fe4059883a";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
---
+++ origin/《人类减贫的中国实践》白皮书（全文）_files/navList.js
@@ -0,0 +1,16 @@
+﻿// JavaScript Document
+// create by geggn 2020/03/10
+document.writeln("<p>");
+document.writeln("<a href='javascript:void(0);' class='navimg'><img src='/Template/6537/Image/6536_20171221175858_0.png'></a>");
+document.writeln("<a href='http://www.scio.gov.cn/index.htm' target='_blank'>首页</a>");
+document.writeln("<a href='http://www.scio.gov.cn/xwbjs/index.htm' target='_blank'>本办介绍</a>");
+document.writeln("<a href='http://www.scio.gov.cn/37234/index.htm' target='_blank'>国新要闻</a>");
+document.writeln("<a href='http://www.scio.gov.cn/xwfbh/index.htm' target='_blank'>新闻发布</a>");
+document.writeln("<a href='http://www.scio.gov.cn/zfbps/index.htm' target='_blank'>政府白皮书</a>");
+document.writeln("<a href='http://www.scio.gov.cn/hzspsx/xwfw/Document/1364232/1364232.htm' target='_blank'>行政审批</a>");
+document.writeln("<a href='http://www.scio.gov.cn/ztk/index.htm' target='_blank'>国新专题</a>");
+document.writeln("<a href='http://www.scio.gov.cn/dfbd/dfbd/index.htm' target='_blank'>地方外宣</a>");
+document.writeln("<a href='http://www.scio.gov.cn/yswxp/index.html' target='_blank'>影视片</a>");
+document.writeln("<a href='http://www.scio.gov.cn/33648/index.htm' target='_blank'>出版物</a>");
+document.writeln("</p>");
+
